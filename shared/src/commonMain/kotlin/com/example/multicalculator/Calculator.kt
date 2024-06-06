695f06f6dc38e3630802a665bc59b3ee2e7b762e
Jay Rana <A00304901@mycambrian.ca>
2024-06-05 21:37:44 -0400

    Initial commit

---
+++ shared/src/commonMain/kotlin/com/example/multicalculator/Calculator.kt
@@ -0,0 +1,25 @@
+package com.example.multicalculator
+
+    class Calculator {
+
+        // Function to add two integers
+        fun add(left: Int, right: Int): Int {
+            return left + right
+        }
+
+        // Function to subtract two integers
+        fun subtract(left: Int, right: Int): Int {
+            return left - right
+        }
+
+        // Function to multiply two integers
+        fun multiply(left: Int, right: Int): Int {
+            return left * right
+        }
+
+        // Function to divide two integers
+        fun divide(left: Int, right: Int): Int {
+            return left / right
+        }
+    }
+
