2e9976a91d18e877c62b1364fdadf2a6e7d77819
Jay Rana <A00304901@mycambrian.ca>
2024-06-14 01:24:18 -0400

    Firstly, I have add the greeting view and default preview in main activity. than, I have implemented calview, calcdisplay, cal row, calview and other methods. In the last I have tested the app and is running sucessfully

--- androidApp/src/main/java/com/example/multicalculator/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/multicalculator/android/MainActivity.kt
@@ -3,11 +3,14 @@ package com.example.multicalculator.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.multicalculator.Greeting
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingView(Greeting().greet())
+                    CalcView()
                 }
             }
         }
@@ -35,6 +38,95 @@ fun GreetingView(text: String) {
 @Composable
 fun DefaultPreview() {
     MyApplicationTheme {
-        GreetingView("Hello, Android!")
+        GreetingView("Welcome to the UI Calculator.")
+    }
+}
+
+@Composable
+fun CalcView() {
+    val displayText = remember { mutableStateOf("0") }
+
+    Column(
+        modifier = Modifier
+            .background(Color.Yellow)
+            .fillMaxSize()
+    ) {
+        Row {
+            CalcDisplay(display = displayText)
+        }
+        Row {
+            Column {
+                for (i in 7 downTo 1 step 3) {
+                    CalcRow(display = displayText, startNum = i, numButtons = 3)
+                }
+                Row {
+                    CalcNumericButton(number = 0, display = displayText)
+                    CalcEqualsButton(display = displayText)
+                }
+            }
+            Column {
+                CalcOperationButton(operation = "/", display = displayText)
+                CalcOperationButton(operation = "*", display = displayText)
+                CalcOperationButton(operation = "+", display = displayText)
+                CalcOperationButton(operation = "-", display = displayText)
+            }
+        }
+    }
+}
+
+@Composable
+fun CalcDisplay(display: MutableState<String>) {
+    Text(
+        text = display.value,
+        modifier = Modifier
+            .height(45.dp)
+            .padding(5.dp)
+            .fillMaxWidth()
+    )
+}
+
+@Composable
+fun CalcRow(display: MutableState<String>, startNum: Int, numButtons: Int) {
+    val endNum = startNum + numButtons
+    Row(modifier = Modifier.padding(0.dp)) {
+        for (i in startNum until endNum) {
+            CalcNumericButton(number = i, display = display)
+        }
+    }
+}
+
+@Composable
+fun CalcNumericButton(number: Int, display: MutableState<String>) {
+    Button(
+        onClick = {
+            if (display.value == "0") {
+                display.value = number.toString()
+            } else {
+                display.value += number.toString()
+            }
+        },
+        modifier = Modifier.padding(4.dp)
+    ) {
+        Text(text = number.toString())
+    }
+}
+
+@Composable
+fun CalcOperationButton(operation: String, display: MutableState<String>) {
+    Button(
+        onClick = { },
+        modifier = Modifier.padding(4.dp)
+    ) {
+        Text(text = operation)
+    }
+}
+
+@Composable
+fun CalcEqualsButton(display: MutableState<String>) {
+    Button(
+        onClick = { display.value = "0" },
+        modifier = Modifier.padding(4.dp)
+    ) {
+        Text(text = "=")
     }
 }
